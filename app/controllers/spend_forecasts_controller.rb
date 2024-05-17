b9b3efad36e731ba8de670c76bf109626f452c2f
gberzins <gustavs.berzins@protonmail.com>
2024-05-17 19:22:36 +0300

    validations, fixes

--- app/controllers/spend_forecasts_controller.rb
+++ app/controllers/spend_forecasts_controller.rb
@@ -1,27 +1,22 @@
 class SpendForecastsController < ApplicationController
+  before_action :spend_forecast, only: [:new, :show]
+
   def index
     @spend_forecasts = SpendForecast.all
-    if params[:sort].present?
-      direction = params[:direction] == 'asc' ? 'asc' : 'desc'
-      @spend_forecasts = @spend_forecasts.order("#{params[:sort]} #{direction}")
-    end
   end
 
-  def new
-    @spend_forecast = spend_forecast
-  end
+  def new;end
 
   def update
-    if spend_forecast.update(spend_forecast_params)
+    spend_forecast.update(spend_forecast_params)
+    if spend_forecast.update(status: :in_progress)
       redirect_to spend_forecast_path(spend_forecast)
     else
       render :new, status: :unprocessable_entity
     end
   end
 
-  def show
-    @spend_forecast = SpendForecast.find(params[:id])
-  end
+  def show;end
 
   private
 
@@ -36,7 +31,6 @@ class SpendForecastsController < ApplicationController
   def spend_forecast_params
     temp_params = params.require(:spend_forecast).permit(:name, :start_date, :end_date)
     temp_params.merge!(budget: process_csv(params[:spend_forecast][:budget_file])) if params[:spend_forecast][:budget_file]
-    temp_params.merge!(status: :in_progress)
     temp_params
   end
 
