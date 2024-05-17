11905a8fee491f80b19094226d3c0b8cf30d62d6
gberzins <gustavs.berzins@protonmail.com>
2024-05-17 22:02:11 +0300

    fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,4 @@ class ApplicationController < ActionController::Base
   def current_user
     @current_user ||= User.first || User.create(channels: %w[facebook radio	billboards])
   end
-  helper_method :current_user
 end
